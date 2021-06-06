431b41562f858913abc265dba173cc1b325d2806
dragsec <draganyes@gmail.com>
2021-06-06 22:21:41 +0200

    3

---
+++ snack3.php
@@ -0,0 +1,59 @@
+<?php
+
+$posts = [
+    '01/01/2000' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Andrea',
+            'text' => 'Testo Prova'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele',
+            'text' => 'Testo Prova'
+        ],
+        [
+            'title' => 'Post 3',
+            'author' => 'Alex',
+            'text' => 'Testo Prova'
+        ]
+    ],
+    '02/01/2000' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Alina',
+            'text' => 'Testo Prova'
+        ]
+    ],
+    '03/01/2000' => [
+        [
+            'title' => 'Post 5',
+            'author' => 'Jennifer',
+            'text' => 'Testo Prova'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Brad',
+            'text' => 'Testo Prova'
+        ]
+        
+    ],
+
+];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 3</title>
+</head>
+<body>
+    
+
+
+</body>
+</html>
